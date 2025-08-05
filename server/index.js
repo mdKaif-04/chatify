0538d49a18c6fa791d7fd93dccd78351272b3f43
mdKaif-04 <md04kaif@gmail.com>
2025-08-05 09:49:29 +0530

    some fix

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const {app,server} = require('./socket/socket.js')
 // const app = express();
 app.use(
   cors({
-    origin: 'https://chatify-kaif.vercel.app' ,
+    origin: process.env.FRONTEND_URL ,
     
     credentials: true,
   })

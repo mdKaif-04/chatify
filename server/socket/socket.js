02a03d5d7093c3278671c41b8cf779d3c47c1b49
mdKaif-04 <md04kaif@gmail.com>
2025-08-05 10:37:06 +0530

    fixes

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL,
+    origin: 'https://chatify-kaif.onrender.com',
     credentials: true,
   },
 });
